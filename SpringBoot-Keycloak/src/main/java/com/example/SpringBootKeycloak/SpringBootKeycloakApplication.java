823d78c4a3316e4759e978d99895cf366fa6057d
Alexander Brockmann <brock021@freenet.de>
2018-07-10 12:10:21 +0200

    finished the configuration

--- SpringBoot-Keycloak/src/main/java/com/example/SpringBootKeycloak/SpringBootKeycloakApplication.java
+++ SpringBoot-Keycloak/src/main/java/com/example/SpringBootKeycloak/SpringBootKeycloakApplication.java
@@ -1,12 +1,11 @@
 package com.example.SpringBootKeycloak;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+
 
 
 @SpringBootApplication
