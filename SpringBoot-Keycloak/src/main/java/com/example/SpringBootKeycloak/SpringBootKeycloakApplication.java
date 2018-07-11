fa2cbe7a26ee54a26f40d3a151e3f0c0cf53c2b8
Alexander Brockmann <brock021@freenet.de>
2018-07-11 12:45:13 +0200

    added login name

--- SpringBoot-Keycloak/src/main/java/com/example/SpringBootKeycloak/SpringBootKeycloakApplication.java
+++ SpringBoot-Keycloak/src/main/java/com/example/SpringBootKeycloak/SpringBootKeycloakApplication.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-
+import java.security.Principal;
 
 @SpringBootApplication
 public class SpringBootKeycloakApplication {
@@ -19,10 +19,11 @@ public class SpringBootKeycloakApplication {
 
 @Controller
 class LoginController{
-	@GetMapping(path = "/login")
-	public String login(){
-				return "login";
-	}
+    @GetMapping(path = "/login")
+    public String login(Principal principal, Model model){
+        model.addAttribute("principal",principal);
+        return "login";
+    }
 
 	@GetMapping(path = "/logout")
 	public String logout(HttpServletRequest request) throws ServletException {
